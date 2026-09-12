c0c8a345bf3f746b750d38b6d78fb9c244c55e71
francoaguirre644 <francoaguirre644@gmail.com>
2026-09-11 23:45:05 -0300

    initialize python grpc client

---
+++ client/client.py
@@ -0,0 +1,27 @@
+import grpc
+
+import turnos_pb2
+import turnos_pb2_grpc
+
+
+def main():
+    channel = grpc.insecure_channel('localhost:5000')
+
+    client = turnos_pb2_grpc.TurnoServiceStub(channel)
+
+    response = client.ListarTurnos(
+        turnos_pb2.ListarTurnosRequest()
+    )
+
+    for turno in response.turnos:
+        print(
+            f'{turno.id} - '
+            f'{turno.paciente} - '
+            f'{turno.fecha} - '
+            f'{turno.hora} - '
+            f'{turno.estado}'
+        )
+
+
+if __name__ == '__main__':
+    main()
