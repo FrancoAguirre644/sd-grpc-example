38149516782acac5ed67564822a412137b934141
francoaguirre644 <francoaguirre644@gmail.com>
2026-09-12 00:30:46 -0300

    feat: configure tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <main class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Gestión de Turnos
+    </h1>
+  </main>
 </template>
