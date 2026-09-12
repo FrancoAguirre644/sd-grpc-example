a04601b7b4023e15ff5c89c01b51363232600ebf
francoaguirre644 <francoaguirre644@gmail.com>
2026-09-11 22:59:17 -0300

    configure nestjs grpc server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { join } from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'turnos',
+        protoPath: join(__dirname, '../../proto/turnos.proto'),
+        url: 'localhost:5000',
+      },
+    },
+  );
+
+  await app.listen();
 }
+
 bootstrap();
