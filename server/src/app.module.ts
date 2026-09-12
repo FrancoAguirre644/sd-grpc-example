d9ae206310b03dde4a29fd87a4e05f6e5a1293aa
francoaguirre644 <francoaguirre644@gmail.com>
2026-09-11 23:34:30 -0300

    configure mysql database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TurnoModule } from './turno/turno.module';
 
 @Module({
-  imports: [TurnoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    }),
+    TurnoModule,
+  ],
 })
 export class AppModule {}
